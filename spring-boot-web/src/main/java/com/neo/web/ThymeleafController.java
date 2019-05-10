f5b7c1bc19021fa1b2a6162c3d95c3c84f91e32f
Zero <35088332+Zerohhhhhh@users.noreply.github.com>
2019-05-10 20:50:40 +0800

    使用Thymeleaf在hello.hmtl添加显示user列表。

--- spring-boot-web/src/main/java/com/neo/web/ThymeleafController.java
+++ spring-boot-web/src/main/java/com/neo/web/ThymeleafController.java
@@ -2,14 +2,21 @@ package com.neo.web;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
+import com.neo.model.User;
+import com.neo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ThymeleafController {
+
+	@Autowired
+	private UserRepository userRepository;
 	
     @RequestMapping("/hi")
 	public String hello(Locale locale, Model model) {
@@ -19,6 +26,9 @@ public class ThymeleafController {
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);        
 		String formattedDate = dateFormat.format(date);
 		model.addAttribute("currentTime", formattedDate);
+
+		List<User> users = userRepository.findAll();
+		model.addAttribute("users",users);
 
 		return "hello";
 	}
--- spring-boot-web/src/test/java/com/neo/model/UserRepositoryTests.java
+++ spring-boot-web/src/test/java/com/neo/model/UserRepositoryTests.java
@@ -26,9 +26,9 @@ public class UserRepositoryTests {
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);        
 		String formattedDate = dateFormat.format(date);
 		
-		userRepository.save(new User("aa1", "aa@126.com", "aa", "aa123456",formattedDate));
-		userRepository.save(new User("bb2", "bb@126.com", "bb", "bb123456",formattedDate));
-		userRepository.save(new User("cc3", "cc@126.com", "cc", "cc123456",formattedDate));
+		userRepository.save(new User("aa1", "aa1@126.com", "aa", "aa123456",formattedDate));
+ 		userRepository.save(new User("bb2", "bb2@126.com", "bb", "bb123456",formattedDate));
+		userRepository.save(new User("cc3", "cc3@126.com", "cc", "cc123456",formattedDate));
 
 //		Assert.assertEquals(9, userRepository.findAll().size());
 		Assert.assertEquals("bb2", userRepository.findByUserNameOrEmail("bb", "xxx126.com").getNickName());
